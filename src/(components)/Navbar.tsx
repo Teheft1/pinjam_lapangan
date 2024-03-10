bb6d4f2a1a9fbe4be5368620cc8b0ebf2aeafbae
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2024-03-10 17:20:09 +0700

    creating the navbar

---
+++ src/(components)/Navbar.tsx
@@ -0,0 +1,46 @@
+// import React from 'react'
+import Link from "next/link";
+import { api } from "~/utils/api";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Navbar = () => {
+  return (
+    <div className=" flex flex-row items-center justify-between bg-black p-4 text-white">
+      <div>LOGO</div>
+      <div className="flex flex-row items-center justify-center gap-10">
+        <Link href={"/admin"}>Admin Dashboard</Link>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/lapangan"}>Lapangan</Link>
+        <Link href={"/pesan"}>Pesan</Link>
+        <Link href={"/histori"}>Histori</Link>
+        <AuthShowcase />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  return (
+    <div className="flex items-center justify-center gap-4">
+      <p className="text-center text-base text-white">
+        {sessionData && <span>{sessionData.user?.name}</span>}
+        {/* {secretMessage && <span> - {secretMessage}</span>} */}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
