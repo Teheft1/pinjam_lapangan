987d4caca726cd69112799dd5340eec3fb2a8dd3
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2024-03-12 13:01:40 +0700

    adding api for create pesan and get lapangan

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { number, z } from "zod";
 
 import {
   createTRPCRouter,
@@ -6,6 +6,7 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 import { db } from "~/server/db";
+import { time } from "console";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
@@ -47,4 +48,54 @@ export const postRouter = createTRPCRouter({
       orderBy: { id_lapangan: "asc" },
     });
   }),
+
+  getlapid: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .query(({ ctx, input }) => {
+      return ctx.db.lapangan.findUnique({
+        where: { id_lapangan: input.id },
+      });
+    }),
+
+  createPesan: protectedProcedure
+    .input(
+      z.object({
+        id_lapangan: z.number(),
+        // tanggal: z.string(),
+        jam: z.date(),
+        durasi: z.date(),
+        subtotal: z.number() || undefined,
+        grand_total: z.number() || undefined,
+        id_invoice: z.string(),
+        catatan: z.string(),
+        // deadline: z.date(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      return ctx.db.transaksi.create({
+        data: {
+          id_lapangan: input.id_lapangan,
+          id_invoice: input.id_invoice,
+          // tanggal: new Date(input.tanggal),
+          // jam: input.jam,
+          // durasi: input.durasi,
+          user_id: ctx.session.user.id,
+          status: 1,
+          created_date: new Date(),
+          created_time: new Date(),
+          subtotal: input.subtotal,
+          diskon: 0,
+          catatan: input.catatan,
+          grand_total: input.grand_total,
+          deadline: new Date(+1),
+          updatedAt: new Date(),
+        },
+      });
+    }),
 });
