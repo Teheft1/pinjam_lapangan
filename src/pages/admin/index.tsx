bd32e2382b76a348da8e3556fd1099233ec4b03c
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2024-03-13 12:29:15 +0700

    preparing admin pages

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  const router = useRouter();
+  const { data: sessionData } = useSession();
+  // if (!sessionData) {
+  //   router.push("/");
+  // }
+  // if (sessionData?.user.role != "Admin") {
+  //   return <div>Forbidden</div>;
+  // }
+  return (
+    <div>
+      Halo
+      {/* <Link></Link>
+    <Link></Link>
+    <Link></Link>
+    <Link></Link>
+    <Link></Link> */}
+    </div>
+  );
 };
 
 export default Admin;
