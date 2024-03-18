7c7713e3a08cc70460be3907285ed8850e6eb5c6
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2024-03-18 11:49:09 +0700

    adding getdata router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { dataRouter } from "~/server/api/routers/get";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  data: dataRouter,
 });
 
 // export type definition of API
